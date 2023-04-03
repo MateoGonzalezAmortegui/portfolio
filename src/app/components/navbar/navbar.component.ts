deca5ccc42bf57e104727aa7dbf55f7ca2ad7084
Mateo Gonzalez Amortegui <mateogonza.2000@hotmail.com>
2023-04-03 10:30:50 -0500

    add features logo and language

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,11 +3,40 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  @Output() toggleMode = new EventEmitter<string>()
-  toggleDarkMode(){
+  @Output() toggleMode = new EventEmitter<string>();
+  toggleDarkMode() {
     this.toggleMode.emit();
+  }
+
+  esp: boolean = true;
+  eng: boolean = false;
+  @Output() toggleIdiom = new EventEmitter<string>();
+  toogleLanguage() {
+    this.esp = !this.esp;
+    this.eng = !this.eng;
+    this.toggleIdiom.emit();
+    this.textFile();
+  }
+
+  aboutMe: string = 'Acerca de mi';
+  projects: string = 'Proyectos';
+  tools: string = 'Herramientas';
+  contact: string = 'Contáctame';
+
+  textFile() {
+    if (this.esp) {
+      this.aboutMe = 'Acerca de mi';
+      this.projects = 'Proyectos';
+      this.tools = 'Herramientas';
+      this.contact = 'Contáctame';
+    } else {
+      this.aboutMe = 'About me';
+      this.projects = 'Projects';
+      this.tools = 'Tools';
+      this.contact = 'Contact me';
+    }
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
 
-  content: [
-    "./src/**/*.{html,ts}"
-  ],
+  content: ["./src/**/*.{html,ts}"],
 
   theme: {
-
     fontFamily: {
       Montserrat: ["Montserrat", "sans-serif"],
     },
 
-    backgroundColor: theme =>({
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      primaryRed: "#CC2D4A",
+      Blues: "#06b6d4",
+      Gray4: "#1F2937",
+      Gray3: "#374151",
+      Gray1: "#D1D5DB",
+      Gray2: "#9ca3af",
+      whites: "#FFFFFF",
+      blacks: "#000000",
+      purples: "#9333ea",
+    }),
 
-      ...theme('colors'),
-      'primaryRed': '#CC2D4A',
-      'Blues': '#06b6d4',
-      'Gray4':'#1F2937',
-      'Gray3':'#374151',
-      'Gray1':'#D1D5DB',
-      'Gray2':'#9ca3af',
-      'whites':'#FFFFFF',
-      'blacks':'#000000',
-      'purples':'#9333ea',
-      }),
-
-      textColor:{
-      'primaryRed': '#CC2D4A',
-      'Blues': '#06b6d4',
-      'Gray4':'#1F2937',
-      'Gray2':'#9ca3af',
-      'Gray3':'#374151',
-      'Gray1':'#D1D5DB',
-      'whites':'#FFFFFF',
-      'blacks':'#000000',
-      'purples':'#9333ea',
-      },
+    textColor: {
+      primaryRed: "#CC2D4A",
+      Blues: "#06b6d4",
+      Gray4: "#1F2937",
+      Gray2: "#9ca3af",
+      Gray3: "#374151",
+      Gray1: "#D1D5DB",
+      whites: "#FFFFFF",
+      blacks: "#000000",
+      purples: "#9333ea",
+    },
 
     extend: {
       backgroundImage: {
-        'hero': "url('https://img.freepik.com/foto-gratis/imagen-primer-plano-programador-trabajando-su-escritorio-oficina_1098-18707.jpg?w=900&t=st=1675108848~exp=1675109448~hmac=2a6557bd9febfa7b4583f13abf87e1fb2208b5666542ded03b85201d34adb68c')",
-        'photo':"url('https://media.licdn.com/dms/image/C4D03AQGYtlczsNRCJg/profile-displayphoto-shrink_200_200/0/1647275698778?e=1680739200&v=beta&t=gjlZGBfT9LY9Ve-bogXPAghzzdQwdlgUyZjtUecZg8U')"
-      }
+        hero: "url('https://img.freepik.com/foto-gratis/imagen-primer-plano-programador-trabajando-su-escritorio-oficina_1098-18707.jpg?w=900&t=st=1675108848~exp=1675109448~hmac=2a6557bd9febfa7b4583f13abf87e1fb2208b5666542ded03b85201d34adb68c')",
+        photo:
+          "url('https://media.licdn.com/dms/image/C4D03AQGYtlczsNRCJg/profile-displayphoto-shrink_200_200/0/1647275698778?e=1680739200&v=beta&t=gjlZGBfT9LY9Ve-bogXPAghzzdQwdlgUyZjtUecZg8U')",
+      },
     },
   },
 
   plugins: [],
-}
+};
