3d2dd4f13fa95e7ae790ec5fcf852cf65c8043d4
Mateo Gonzalez Amortegui <mateogonza.2000@hotmail.com>
2023-04-04 15:01:34 -0500

    add idioms

--- src/app/components/exp-work/exp-work.component.ts
+++ src/app/components/exp-work/exp-work.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IdiomService } from 'src/app/services/idiom.service';
 
 @Component({
   selector: 'app-exp-work',
@@ -11,38 +12,13 @@ export class ExpWorkComponent {
   toogleExp() {
     this.select1 = true;
     this.select2 = false;
-    this.changeJob();
   }
   toogleExp2() {
     this.select2 = true;
     this.select1 = false;
-    this.changeJob();
   }
 
-  tittle: string = 'Semillero JR';
-  p1: string =
-    '✅ Desarrollo e implementación de Microservicios, frontend y desarrollo mobile bajo metodologías Scrum.';
-  p2: string =
-    '📝1. Mantenimiento y depuración de diferentes proyectos para mejorar el rendimiento y tener un código limpio.';
-  p3: string =
-    '📝2. Desarrollo de implementaciones o servicios para clientes de la red.';
+  constructor(private idiom: IdiomService) {}
 
-  changeJob() {
-    if (this.select1) {
-      this.tittle = 'Semillero JR';
-      this.p1 =
-        '✅ Desarrollo e implementación de Microservicios, frontend y desarrollo mobile bajo metodologías Scrum.';
-      this.p2 =
-        '📝1. Mantenimiento y depuración de diferentes proyectos para mejorar el rendimiento y tener un código limpio.';
-      this.p3 =
-        '📝2. Desarrollo de implementaciones o servicios para clientes de la red.';
-    }
-
-    if (this.select2) {
-      this.tittle = 'Monitor';
-      this.p1 = '✅ Desarrollo.';
-      this.p2 = '📝1. Mantenimiento .';
-      this.p3 = '📝2. Desarrollo.';
-    }
-  }
+  language$ = this.idiom.language$;
 }
