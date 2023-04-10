8ca6ac2fd5f95eab6b73ece658856c3e2d38f0e5
Mateo Gonzalez Amortegui <mateogonza.2000@hotmail.com>
2023-04-10 17:06:04 -0500

    fix photo hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,7 @@ module.exports = {
       backgroundImage: {
         hero: "url('https://img.freepik.com/foto-gratis/imagen-primer-plano-programador-trabajando-su-escritorio-oficina_1098-18707.jpg?w=900&t=st=1675108848~exp=1675109448~hmac=2a6557bd9febfa7b4583f13abf87e1fb2208b5666542ded03b85201d34adb68c')",
         photo:
-          "url('https://media.licdn.com/dms/image/C4D03AQGYtlczsNRCJg/profile-displayphoto-shrink_200_200/0/1647275698778?e=1680739200&v=beta&t=gjlZGBfT9LY9Ve-bogXPAghzzdQwdlgUyZjtUecZg8U')",
+          "url('https://media.licdn.com/dms/image/C4D03AQGYtlczsNRCJg/profile-displayphoto-shrink_200_200/0/1647275698778?e=1686787200&v=beta&t=wQXLhwK-13DVJfVnjqC0dCXK1KW6PVR-XpdLuHoa-9E')",
       },
     },
   },
